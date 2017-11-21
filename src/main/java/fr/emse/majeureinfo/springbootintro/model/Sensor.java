d29e640838ce2ac6d2b600c19a159f5e40f3814a
SuperValou96 <valentin.pouzet@etu.emse.fr>
2017-11-21 11:27:30 +0100

    fix sensor.java

--- src/main/java/fr/emse/majeureinfo/springbootintro/model/Sensor.java
+++ src/main/java/fr/emse/majeureinfo/springbootintro/model/Sensor.java
@@ -4,13 +4,25 @@ import javax.persistence.*;
 
 @Entity
 @SuppressWarnings("serial")
+@Table(name="SENSOR") // Not compulsory but better to name the table
 public class Sensor {
+// The @ are very important and were missing so it wasn't linked in the database
 
+    @Id
+    @GeneratedValue
     private Long id;
 
+    @Column(nullable = false)
     private Integer signal;
 
+    @Enumerated(EnumType.STRING)
     private Status status;
+
+    // Needs a public void constructor
+    @SuppressWarnings("unused")
+    public Sensor(){
+    }
+
 
     public Sensor(Integer signal, Status status) {
         this.signal = signal;
